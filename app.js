de34849926cfb8a9c330e327bdcc79371ebe4a79
David Kavanaugh <davidkavanaugh@outlook.com>
2020-03-31 09:18:01 -0700

    express, react, node setup complete

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require( 'body-parser');
 const config = require('./config');
+const dns = require('dns');
+const uuid = require('short-uuid');
 
 const { MONGO_URI } = config;
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // DB Config
 const db = `${MONGO_URI}`;
@@ -23,5 +25,61 @@ mongoose
   // Adding new mongo url parser
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+const links = [];
+
+// Create Database Entry
+app.post('/api/shorturl/new', (req, res) => {
+  const { url } = req.body;
+
+  console.log(url);
+
+  const regex =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  
+  const noHTTPSurl = url.replace(regex, '');
+
+  // check if this url is valid
+  dns.lookup(noHTTPSurl, (err) => {
+
+    if (err) {
+      return res.json({
+        error: "invalid URL"
+      });
+    } else {
+      //increment id
+      let id = uuid.generate();
+
+      //create new entry for arr
+      const link = {
+        original_url: url,
+        short_url: `${id}`
+      };
+      links.push(link);
+
+      console.log(links);
+
+      //return new entry
+      return res.json(link)
+    }
+  });
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params;
+
+  console.log('id from query:', id)
+
+  const link = links.find(l => l.short_url === id);
+
+  console.log('link found', link);
+
+  if (link) {
+    return res.redirect(link.original_url);
+  } else {
+    return res.json({
+      error: "No short url"
+    });
+  }
+})
 
 module.exports = app;
--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'https://www.freecodecamp.org'
+    }
+};
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert('A URL was submitted: ' + this.state.value)
+    event.preventDefault();
+  }
+
   render() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>MERN Stack Boilerplate</h3>
+        <h3>URL Shortener</h3>
+        <form action="http://localhost:3001/api/shorturl/new" method="POST">
+          <label>URL to be shortened</label><br />
+          <input id="url_input" type="text" name="url" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="POST URL" />
+        </form>
       </header>
     </div>
   );
